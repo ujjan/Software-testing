2dca040727ad395e99af3fccb0d93c4fd52cb49b
AyazAli <ayaz.ujjan@yahoo.com>
2015-11-05 11:43:52 +0100

    getting start with.

---
+++ SoftwareTesting/src/Assignment/Tessting.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Tessting {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ TDD/src/testing/Test1.java
@@ -0,0 +1,16 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test() {
+		
+		
+		
+	}
+
+}
