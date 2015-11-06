74ac284a7707afb1b741463b85bd1fc8c119c49e
AyazAli <ayaz.ujjan@yahoo.com>
2015-11-06 16:52:18 +0100

    REFEC

--- SoftwareTesting/src/Assignment/Game.java
+++ SoftwareTesting/src/Assignment/Game.java
@@ -3,14 +3,20 @@ package Assignment;
 import java.util.Random;
 
 public class Game {
-
+	
+	
 	public int rand() {
 		
 		Random number = new Random();
 		
 		int num = 1+number.nextInt(100);
-		
 		return num;
+		
+	}
+
+	public static void wecomeToTheGame() {
+		System.out.println("welcome to the game please "
+				+ "inter your guess between 1 to 99");
 		
 	}
 
