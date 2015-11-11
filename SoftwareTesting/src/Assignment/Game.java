e519e2f8333a0e25d38312c9c41f17a6fa7f4362
AyazAli <ayaz.ujjan@yahoo.com>
2015-11-11 13:07:51 +0100

    GREEN

--- SoftwareTesting/src/Assignment/Game.java
+++ SoftwareTesting/src/Assignment/Game.java
@@ -4,13 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
 	
 	
-	public int rand() {
+	Scanner scan = new Scanner(System.in);
+	
+   
+	public static int rand() {
 		
 		Random number = new Random();
+		
+		
 		
 		int num = 1+number.nextInt(100);
 		return num;
@@ -21,14 +27,33 @@ public class Game {
 		System.out.println("welcome to the game please "
 				+ "guess the number between 1 to 99");
 		
+		
 	}
 
-	public int readTheScanner() throws NumberFormatException, IOException {
-		BufferedReader console = new BufferedReader(new InputStreamReader(
-	            System.in));
-	    return Integer.parseInt(console.readLine());
+//	public static int readTheScanner() throws NumberFormatException, IOException {
+//		BufferedReader console = new BufferedReader(new InputStreamReader(
+//	            System.in));
+//	    return Integer.parseInt(console.readLine());
+//	}
+	
+	
+	public static int numberOfChoices(){
+		
+		int i;
+		for(i=0; i<10; i++){
+			
+			
+		}
+		
+		return i;
+		
+		
+		 
+		
 	}
+	
 
+	
 	
 	
 	
